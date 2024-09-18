69e3ed568635ae66594b1404fefa37cdb9abcaa7
Casparml <95343459+casparml@users.noreply.github.com>
2024-09-18 12:04:42 +0200

    big add

--- assets/js/app.js
+++ assets/js/app.js
@@ -117,11 +117,12 @@ window.onload = function () {
 	  toggle(isPageActive)
 	})
   
+    // 2 of 3 .
 	function toggle(isPageActive) {
 	  if (isPageActive) {
-		favicon.href = './images/logo.png'
+		favicon.href = '../images/logo.png'
 	  } else {
-		favicon.href = './images/logo-away.png'
+		favicon.href = '../images/logo-away.png'
 	  }
 	}
 }
